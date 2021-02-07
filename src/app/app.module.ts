672902f75b6ea61f80061d8a46ef527b5a207654
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2021-02-07 21:36:23 +0900

    create user-list-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { UserListItemComponent } from './view/user-list-item/user-list-item.component';
+import { UserListComponent } from './view/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListItemComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
