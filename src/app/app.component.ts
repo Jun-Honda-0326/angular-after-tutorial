33a4685e62b3077a0d5eaed333cb3ea210953356
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2021-02-07 20:58:13 +0900

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from './user';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  users: User[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+     this.http.get<{ data: User[] }>('https://reqres.in/api/users')
+     .subscribe((res) => {
+       this.users = res.data;
+     });
+  }
+
+}
